7a0e32e78f5a97b786200e84856aab684376760d
Milkuri <vmilkuri@trcsolutions.com>
2025-12-30 12:39:29 +0530

    added reusabole dropdown

--- build.js
+++ build.js
@@ -19,6 +19,7 @@ const js =
   fs.readFileSync("src/consts/default.js", "utf8") +
   fs.readFileSync("src/components/button.js", "utf8") +
   fs.readFileSync("src/components/input.js", "utf8") +
+  fs.readFileSync("src/components/dropdown.js", "utf8") +
   fs.readFileSync("src/core/init.js", "utf8");
 
 fs.writeFileSync("dist/pp-ui-core.min.js", js);
--- src/core/init.js
+++ src/core/init.js
@@ -2,6 +2,7 @@ window.PP_UI = {
   init() {
     if (typeof initButtons === "function") initButtons();
     if (typeof initInputs === "function") initInputs();
+    if (typeof initDropdowns === "function") initDropdowns();
   }
 };
 
