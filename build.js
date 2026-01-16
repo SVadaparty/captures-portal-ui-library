a0f1384d41492f3e8297e2f15495c6344097f609
Kumar <chanchal.kumar@trcsolutions.com>
2026-01-16 16:03:09 +0530

    toast

--- build.js
+++ build.js
@@ -26,6 +26,7 @@ const js =
   fs.readFileSync("src/components/fileUpload.js", "utf8") +
   fs.readFileSync("src/components/view_table.js", "utf8") +
   fs.readFileSync("src/components/model.js", "utf8") +
+  fs.readFileSync("src/components/toast.js", "utf8") +
   fs.readFileSync("src/components/multiDropdown.js", "utf8") +
   fs.readFileSync("src/core/init.js", "utf8");
 
